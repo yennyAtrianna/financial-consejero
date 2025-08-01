15e2f0c404385fbacde024002f1a0aad09e0f79d
Yenny Gonzalez <ygonzalez@atrianna.com>
2025-08-01 16:28:37 -0500

    chore: proyecto inicial con Vite, React, TS y Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-primary">
+      <h1 className="text-4xl text-white">
+        ¡Tailwind funciona correctamente!
+      </h1>
+    </div>
+  )
+}
